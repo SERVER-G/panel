affada223b8f5fb2cf06d4fd1c35d994954dd7d9
MG8853 <qq3082499405@icloud.com>
2022-06-15 21:08:20 +0900

    add

--- resources/scripts/components/dashboard/DashboardContainer.tsx
+++ resources/scripts/components/dashboard/DashboardContainer.tsx
@@ -142,6 +142,10 @@ export default () => {
                     )}
                 </Pagination>
             }
+
+            with:
+
+            <SortableList />
         </PageContentBlock>
     );
 };
